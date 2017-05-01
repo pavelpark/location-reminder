1236bd5b22c8e5c716a97cb2c311e3da814800c5
Pavel Parkhomey <pavelpark@aol.com>
2017-05-01 10:46:45 -0700

    Initial Commit

---
+++ LocationReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminders
+//
+//  Created by Pavel Parkhomey on 5/1/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationReminders
+//
+//  Created by Pavel Parkhomey on 5/1/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
