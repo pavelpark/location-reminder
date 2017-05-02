46f2cdaf8a245a2e721ea7b95825e1357340000f
Pavel Parkhomey <pavelpark@aol.com>
2017-05-02 10:54:35 -0700

    Added a new Controller and a new window in the story board.

---
+++ LocationReminders/AddReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReminderViewController.h
+//  LocationReminders
+//
+//  Created by Pavel Parkhomey on 5/2/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReminderViewController : UIViewController
+
+@end
