9bf5eb6c5e76162db98cc40fc91a8b5d37a31bc8
Pavel Parkhomey <pavelpark@aol.com>
2017-05-02 10:15:30 -0700

    We added the distance for the range in meters.

--- LocationReminders/ViewController.m
+++ LocationReminders/ViewController.m
@@ -30,22 +30,28 @@
 
 -(void)requestsPermissions{
     self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.distanceFilter = 100; //In meters
+    
     [self.locationManager requestAlwaysAuthorization];
+    
+    [self.locationManager startUpdatingLocation];
 }
-
+//IKEA Store
 - (IBAction)location1Pressed:(id)sender {
     CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6566674, -122.351096);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
     
     [self.mapView setRegion:region animated:YES];
 }
-
+//GoPro HeadQuaters
 - (IBAction)location2Pressed:(id)sender {
     CLLocationCoordinate2D coordinateTwo = CLLocationCoordinate2DMake(37.53451769999999, -122.33128290000002);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinateTwo, 500.0, 500.0);
     
     [self.mapView setRegion:region animated:YES];
 }
+//Red Bull HeadQuaters
 - (IBAction)location3Pressed:(id)sender {
     CLLocationCoordinate2D coordinateThree = CLLocationCoordinate2DMake(33.7921386, -84.3696071);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinateThree, 500.0, 500.0);
