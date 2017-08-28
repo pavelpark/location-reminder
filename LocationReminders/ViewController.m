0190b9a39c96b498d26b63538f4e3731e69c53d6
Pavel Parkhomey <pavelpark@aol.com>
2017-08-28 14:51:48 -0700

    Add current location button to mapView.
    This resolves #22

--- LocationReminders/ViewController.m
+++ LocationReminders/ViewController.m
@@ -183,6 +183,13 @@
 
 //MARK: User actions
 
+//Current location button.
+
+- (IBAction)currentLocationTapped:(id)sender {
+    
+    [self.mapView setRegion: MKCoordinateRegionMake(self.locationManager.location.coordinate, MKCoordinateSpanMake(0.01f, 0.01f)) animated:YES];
+}
+
 //LongPress for the pin to drop.
 - (IBAction)userLongPressed:(UILongPressGestureRecognizer *)sender {
     
@@ -201,11 +208,6 @@
         
         [self.mapView addAnnotation:newPoint];
     }
-}
-//Zooms on the user when they hit currentLocation button.
-- (IBAction)currentLocation:(id)sender {
-    
-     [self.mapView setRegion: MKCoordinateRegionMake(self.locationManager.location.coordinate, MKCoordinateSpanMake(0.01f, 0.01f)) animated:YES];
 }
 
 - (IBAction)signOut:(id)sender {
