998eb8367d8c01a15125620f961ea54f6147f5ea
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-09-14 11:16:11 -0700

    Add background image to Parse login view

--- LocationReminders/LRViewController.m
+++ LocationReminders/LRViewController.m
@@ -10,14 +10,22 @@
 
 @interface LRViewController ()
 
+@property UIImageView *backgroundView;
+
 @end
 
 @implementation LRViewController
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor darkGrayColor];
+    
+    self.backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"sylwia_bartyzel_442"]];
+    self.backgroundView.contentMode = UIViewContentModeScaleAspectFill;
+    [self.view insertSubview:self.backgroundView atIndex:0];
     
     UIImageView *logoView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LoginLogoClear"]];
     logoView.contentMode = UIViewContentModeScaleAspectFit;
@@ -28,6 +36,11 @@
     PFLogInFieldsUsernameAndPassword |
     PFLogInFieldsPasswordForgotten;
     
+}
+
+- (void) viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.backgroundView.frame = CGRectMake(0, 0, self.logInView.frame.size.width, self.logInView.frame.size.height);
 }
 
 @end
