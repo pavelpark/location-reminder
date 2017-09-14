768608dd05c690686692bf452e42f1fadc336fb2
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-09-14 13:25:58 -0700

    Add custom LogoView class, adjust logo layout

--- LocationReminders/LRViewController.m
+++ LocationReminders/LRViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "LRViewController.h"
+#import "LogoView.h"
 
 @interface LRViewController ()
 
 @property UIImageView *backgroundView;
+@property LogoView *logoView;
 
 @end
 
@@ -21,15 +23,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Set background color and image
     self.view.backgroundColor = [UIColor darkGrayColor];
-    
     self.backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"sylwia_bartyzel_442"]];
     self.backgroundView.contentMode = UIViewContentModeScaleAspectFill;
     [self.view insertSubview:self.backgroundView atIndex:0];
     
-    UIImageView *logoView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"LoginLogoClear"]];
-    logoView.contentMode = UIViewContentModeScaleAspectFit;
-    self.logInView.logo = logoView;
+    // Create logo view and assign as new logInview
+    self.logoView = [[LogoView alloc] initWithFrame:CGRectMake(0, 0, 400, 200)];
+    self.logInView.logo = self.logoView;
     
     self.fields = PFLogInFieldsLogInButton |
     PFLogInFieldsSignUpButton |
@@ -40,7 +42,18 @@
 
 - (void) viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
+    
+    // Layout background view to fit screen
     self.backgroundView.frame = CGRectMake(0, 0, self.logInView.frame.size.width, self.logInView.frame.size.height);
+    
+    // Layout logo view
+    [self.logInView.logo sizeToFit];
+    CGRect logoFrame = self.logInView.logo.frame;
+    // Calculate new Y position based on size of logo view.
+    // The offset is somewhat arbitrary; the PFLoginViewController superclass is appears to be overriding the logoView's
+    //  initial height, and basing the position of the usernameField on that. We adjusted constants until it looked good.
+    CGFloat logoFrameOriginY = self.logInView.usernameField.frame.origin.y - logoFrame.size.height - 60;
+    self.logInView.logo.frame = CGRectMake(logoFrame.origin.x, logoFrameOriginY, self.logInView.frame.size.width, 100);
 }
 
 @end
